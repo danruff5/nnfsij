b589fca5a35a0f83908f78ba2bc04cfa89d2a267
danruff5 <dckrauskopf@gmail.com>
2021-03-07 07:30:17 -0500

    Removed old code for source as its in history now

--- src/main/java/ca/danruff5/nnfsij/main/Application.java
+++ src/main/java/ca/danruff5/nnfsij/main/Application.java
@@ -1,11 +1,8 @@
 package ca.danruff5.nnfsij.main;
 
-import ca.danruff5.nnfsij.main.activations.ActivationFunction;
 import ca.danruff5.nnfsij.main.activations.ActivationFunctionFactory;
 import ca.danruff5.nnfsij.main.layers.DenseLayer;
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -13,54 +10,6 @@ public class Application {
     
     
     public static void main(String[] args) {
-        /*List<Double> inputs1 = Arrays.asList(
-                1.0, 
-                2.0, 
-                3.0, 
-                2.5
-        );
-        List<Double> inputs2 = Arrays.asList(
-                2.0, 
-                5.0, 
-                -1.0, 
-                2.0
-        );
-        List<Double> inputs3 = Arrays.asList(
-                -1.5, 
-                2.7, 
-                3.3, 
-                -0.8
-        );
-        List<List<Double>> X = Arrays.asList(inputs1, inputs2, inputs3);*/
-        
-        /*Layer layer1 = new Layer(
-                new Neuron(2.0, 0.2, 0.8, -0.5, 1.0),
-                new Neuron(3.0, 0.5, -0.91, 0.26, -0.5),
-                new Neuron(0.5, -0.26, -0.27, 0.17, 0.87)
-        );
-        List<List<BigDecimal>> layer1_outputs = layer1.outputBatch(inputs1, inputs2, inputs3);
-        Layer layer2 = new Layer(
-                new Neuron(-1.0, 0.1, -0.14, 0.5),
-                new Neuron(2.0, -0.5, 0.12, -0.33),
-                new Neuron(-0.5, -0.44, 0.73, -0.13)
-        );
-        System.out.println(layer2.outputBatch(layer1_outputs));*/
-        
-        /*DenseLayer layer1a = new DenseLayer("RELU|2.0,0.2,0.8,-0.5,1.0|3.0,0.5,-0.91,0.26,-0.5|0.5,-0.26,-0.27,0.17,0.87|");
-        DenseLayer layer2a = new DenseLayer("RELU|-1.0,0.1,-0.14,0.5|2.0,-0.5,0.12,-0.33|-0.5,-0.44,0.73,-0.13|");
-        
-        //DenseLayer layer1 = new DenseLayer(4, 3);
-        List<List<Double>> layer1_output = layer1a.forward(X);
-        //DenseLayer layer2 = new DenseLayer(3, 3);
-        List<List<Double>> layer2_output = layer2a.forward(layer1_output);
-        
-        System.out.println(layer1_output.get(0));
-
-        ActivationFunction function = ActivationFunctionFactory.create("SOFTMAX");
-        List<Double> softMaxOut = function.activate(layer1_output.get(0));
-        System.out.println(softMaxOut);
-        System.out.println(softMaxOut.stream().mapToDouble(Double::doubleValue).sum());*/
-        
         Dataset dataset = new Dataset();
 
         DenseLayer dense1 = new DenseLayer(ActivationFunctionFactory.create("RELU"), 2, 3);
